f795a11aa3d9b58524750f3063673135b6ed05df
Andries Spies <andries.spies@gmail.com>
2015-04-07 00:25:57 +0200

    Completed sample server side app.

---
+++ sample-app/src/main/java/org/andriesfc/h2/spring/embeddedsample/SampleApp.java
@@ -0,0 +1,33 @@
+package org.andriesfc.h2.spring.embeddedsample;
+
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.concurrent.CountDownLatch;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class SampleApp {
+
+    @Bean(destroyMethod = "countDown")
+    CountDownLatch shutdown() {
+        return new CountDownLatch(1);
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+
+        ConfigurableApplicationContext context = new SpringApplicationBuilder()
+                .sources(SampleApp.class)
+                .registerShutdownHook(true)
+                .headless(true)
+                .web(false)
+                .run(args);
+
+        CountDownLatch shutdown = context.getBean(CountDownLatch.class);
+        shutdown.await();
+    }
+}
